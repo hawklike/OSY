d69eaa1a952877526c985f9f5f8a5c7896b38409
Jan Steuer <jan.steuer@centrum.cz>
2019-03-06 14:28:30 +0100

    Initial commit

---
+++ proseminare/p01-examples/02_thr.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+typedef struct TThrArg 
+ {
+   int        m_Max;
+   int        m_Num;
+   pthread_t  m_ThrID;
+ } TTHRARG;
+ 
+/* Funkce vlakna - vypise cisla v danem rozsahu. Parametry dostane 
+ * jako netypovy ukazatel void *. Protoze vlakno vytvarime a vime,
+ * ze tento netypovy ukazatel ve skutecnosti ukazuje na nami pripravenou
+ * strukturu TTHRARG, pretypujeme tento ukazatel a pak pomoci typovaneho
+ * ukazatele data pracujeme s predanymi hodnotami.
+ */
+void * thrFunc ( void * arg )
+{
+  int i;
+  TTHRARG  * data = (TTHRARG *)arg;
+  
+  for ( i = 0; i < data -> m_Max; i ++ )
+    printf ( "%d: %d\n", data -> m_Num, i );
+  return NULL;
+}
+
+int main ( int argc, char * argv [] )
+{
+  TTHRARG      * arg;
+  pthread_attr_t thrAttr;
+  int            i, thr;
+  void         * dummy;
+
+  /* Vytvarime vlakno pro kazdy parametr z prikazove radky
+   */
+  thr = argc - 1;
+  if ( thr <= 0 )
+  {
+    printf ( "Usage: %s n1 [n2 [n3 [...]]]\n", argv[0] );
+    return 1;
+  }
+
+  /* Pole parametru pro vlakna. 
+   */
+  arg = (TTHRARG *) malloc ( thr * sizeof ( *arg ) );
+
+  for ( i = 0; i < thr; i ++ )
+  {
+    if ( sscanf ( argv[i+1], "%d", &arg[i] . m_Max) != 1 )
+    {
+      free ( arg );
+      printf ( "Invalid argument %d\n", i + 1 );
+      return 1;
+    }
+    arg[i] . m_Num = i;
+  }
+
+  /* Atributy vlakna (ciste reseni, na vlakno lze cekat pomoci pthread_join). 
+   */ 
+  pthread_attr_init ( &thrAttr );
+  pthread_attr_setdetachstate ( &thrAttr, PTHREAD_CREATE_JOINABLE );
+  
+  /* Vytvareni vlaken
+   */  
+  for ( i = 0; i < thr; i ++ )
+    if ( pthread_create ( &arg[i] . m_ThrID, &thrAttr, thrFunc, &arg[i] ) )
+    {
+      free ( arg );
+      perror ( "Create thread\n" );
+      pthread_attr_destroy ( &thrAttr );
+      return 1;
+    }
+  pthread_attr_destroy ( &thrAttr );
+
+  /* Cekani na dokonceni
+   */
+  for ( i = 0; i < thr; i ++ )
+    pthread_join ( arg[i].m_ThrID, &dummy );
+
+  /* Uklid
+   */
+  free ( arg );
+  return 0; 
+}
