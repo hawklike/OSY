b56144cfaff62904f8c0587632d27cf24e0f58c8
Jan Steuer <jan.steuer@centrum.cz>
2019-03-06 16:07:20 +0100

    Added task 1

---
+++ cviceni1/cmake-build-debug/CMakeFiles/cviceni1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cviceni1.dir/main.cpp.o"
+  "cviceni1.pdb"
+  "cviceni1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cviceni1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cviceni1/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+
+class CThreadManager
+{
+public:
+    void solve(const std::vector<std::thread> &threads, int start, int end)
+    {
+
+    }
+
+
+private:
+};
+
+int main(int argc, const unsigned int args[])
+{
+    uint32_t m = args[0];
+    uint16_t n = static_cast<uint16_t>(args[1]);
+
+    std::vector<std::thread> threads;
+
+
+    return 0;
+}
