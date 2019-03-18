1ce6183b626c62d4169b301cbacc0c77f5a63071
Jan Steuer <jan.steuer@centrum.cz>
2019-03-18 11:22:57 +0100

    Initial commit

---
+++ progtest1/sync_threads/cmake-build-debug/CMakeFiles/sync_threads.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/hawklike/skola/OSY/progtest1/sync_threads/sample_tester.cpp" "/home/hawklike/skola/OSY/progtest1/sync_threads/cmake-build-debug/CMakeFiles/sync_threads.dir/sample_tester.cpp.o"
+  "/home/hawklike/skola/OSY/progtest1/sync_threads/solution.cpp" "/home/hawklike/skola/OSY/progtest1/sync_threads/cmake-build-debug/CMakeFiles/sync_threads.dir/solution.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ progtest1/sync_threads/cmake-build-debug/CMakeFiles/sync_threads.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sync_threads.dir/solution.cpp.o"
+  "CMakeFiles/sync_threads.dir/sample_tester.cpp.o"
+  "sync_threads.pdb"
+  "sync_threads"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sync_threads.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
