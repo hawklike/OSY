b6a9a09d18270f75e90cab6413501277f3d512dc
Jan Steuer <jan.steuer@centrum.cz>
2019-05-25 16:25:14 +0200

    Deadlock example

--- zkouska/fork/main.cpp
+++ zkouska/fork/main.cpp
@@ -1,7 +1,34 @@
 #include <iostream>
+#include <zconf.h>
+#include <mutex>
+#include <thread>
+
+using namespace std;
+
+int g_X = 120;
+mutex g_M[4];
+
+void foo(int pos){
+    lock_guard<mutex>l0 (g_M[pos % 4]);
+    lock_guard<mutex>l1 (g_M[pos+1 % 4]);
+    lock_guard<mutex>l2 (g_M[pos+2 % 4]);  //<---- zde by mělo nejspíš být pos+2, jinak mi to nedává smysl (zřejmě jde pouze o překlep)
+    std::cout << g_X << std::endl;
+    g_X+=pos;                               //   ^^ Nemyslim si, ze musi jit nutne o preklep. Pokud to uzamknu 2x stejnym mutexem, tak by podle me melo dojit k deadlocku.
+}
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+    thread t1(foo, 9);
+    thread t2(foo, 18);
+    thread t3(foo, 21);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    std::cout << g_X << std::endl;
+
 }
+
+
